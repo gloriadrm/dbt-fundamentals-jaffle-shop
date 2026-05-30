3eb96ca021d6c1bb8c2af795f9cc290bc8721abc
Gloria <gloriadelriomarquez@outlook.com>
2026-05-30 15:16:02 +0000

    Feat: Add macro cents_to_dollars and jinja calculation in intermediate orders sql

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,19 +1,17 @@
-with 
-payments as (
-    select * from {{ source('stripe', 'payment') }}
-),
+with
+    payments as (select * from {{ source("stripe", "payment") }}),
 
+    transformed as (
+        select
+            id as payment_id,
+            orderid as order_id,
+            created as payment_created_at,
+            {{ cents_to_dollars("amount", 4)}} as payment_amount,
+            paymentmethod as payment_method,
+            status as payment_status,
+            _batched_at
+        from payments
+    )
 
-transformed as ( 
-    select 
-        id as payment_id,   
-        orderid as order_id,
-        created as payment_created_at, 
-        amount as payment_amount,
-        paymentmethod as payment_method,
-        status as payment_status,
-        _batched_at
-    from payments
-)
-
-select * from transformed
+select *
+from transformed
