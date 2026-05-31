2297191d80404269ca1ae09b2ae1cfd7460767df
Gloria <gloriadelriomarquez@outlook.com>
2026-05-31 12:55:15 +0000

    Fix sintax error in customize_schema_by_environment

--- macros/customize_schema_by_environment.sql
+++ macros/customize_schema_by_environment.sql
@@ -3,7 +3,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- set env = env_var{'DBT_ENV_NAME'} -%}
+    {%- set env = env_var('DBT_ENV_NAME') -%}
 
     {%- if custom_schema_name is none or env != 'prod' -%}
 
